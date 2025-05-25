5da5c66ff91aed81e0e213c03ca9718ace69179c
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-05-25 12:34:03 +0530

    Add Python scripts for input handling, arithmetic operations, and variable type checking

---
+++ Chapter2/problem2.py
@@ -0,0 +1,5 @@
+#2.write a python program to find a remainder when a number is divided by z
+
+a=34
+b=5
+print("Remainder when", a, "is divided by", b, "is:", a % b)
---
+++ Chapter2/problem3.py
@@ -0,0 +1,4 @@
+# 3.check the type of a variable assigned using input() function
+
+a=input("Enter the value of a : ")
+print(type(a))
