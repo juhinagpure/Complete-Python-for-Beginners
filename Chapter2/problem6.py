1dc5e675feb1c7f86cadffd90a292bd566af27c6
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-05-25 12:40:58 +0530

    Add Python programs to calculate average and square of user-entered numbers

---
+++ Chapter2/problem6.py
@@ -0,0 +1,10 @@
+# Write a python program to calculate the square of a number entered by the user
+
+a=int(input("Enter your number : "))
+
+print ("The square of the number is :",a**2)
+print ("The square of the number is :",a**a)
+
+# print ("The square of the number is :",a^2) # This line is incorrect for squaring a number in Python, as it uses the bitwise XOR operator.
+
+
