90bb619f70896105b65617ae6490429b09f4f860
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-05-27 13:56:08 +0530

    Add examples for string manipulation and letter formatting

---
+++ chapter3/problem4.py
@@ -0,0 +1,4 @@
+name="Juhi is a good girl and "
+
+print(name.replace("   " , "   "))
+print(name) #strings are immutable which means that you cannot change them by running functions on them
---
+++ chapter3/problem5.py
@@ -0,0 +1,2 @@
+letter="Dear Juhi,\n\tThis python course s nice .\nThanks!"
+print(letter)
