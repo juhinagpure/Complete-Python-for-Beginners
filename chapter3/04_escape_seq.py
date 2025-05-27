2cde09aabf8970315171cd844d25e6d59b3e3ef6
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-05-27 13:32:32 +0530

    Add escape sequence demonstration and user greeting program

---
+++ chapter3/04_escape_seq.py
@@ -0,0 +1,2 @@
+a="juhi is a good girl \n but not a good \"girl\""
+print(a)
---
+++ chapter3/problem1.py
@@ -0,0 +1,2 @@
+# Write a python program to display a user entered name followed by good afternoon using input() function 
+
