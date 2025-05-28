26243a9da76912b7a2ff17f2974bc3ec0b063913
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-05-28 22:08:16 +0530

    Update dictionary methods example to demonstrate error handling and retrieval of non-existent keys

--- chapter5/02_dict_methods.py
+++ chapter5/02_dict_methods.py
@@ -7,5 +7,8 @@ marks = {
 # print(marks.items())
 # print(marks.keys())
 # print(marks.values())
-marks.update({"harry": 99})
+# marks.update({"harry": 99})
 print(marks)
+
+print(marks.get("harry2")) #prints None
+print(marks["harry2"]) #Returns an error
