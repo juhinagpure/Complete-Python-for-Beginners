9409635e47741423308eb9881b8503e5ae856c44
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-05-29 13:24:55 +0530

    Add programs for Hindi-English dictionary translation and unique number display

---
+++ chapter5/problem1.py
@@ -0,0 +1,9 @@
+# 1. Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up! 
+
+words={
+    "madad":"help",
+    "kursi":"chair",
+     "paani":"water",
+}
+word=input("Enter the Hindi word you want to translate: ")
+print(words[word])
