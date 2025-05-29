d02e8acadd2c642e3cab0613c6e2d004941debef
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-05-29 13:47:25 +0530

    Add examples for dictionary creation and set behavior in chapter 5

---
+++ chapter5/problem6.py
@@ -0,0 +1,22 @@
+# 6. Create an empty dictionary. Allow 4 friends to enter their favorite language as value and use key as their names. Assume that the names are unique
+d={}
+name=input("Enter friends name:")
+lang=input("Enter language name:")
+
+d.update({name:lang})
+name=input("Enter friends name:")
+lang=input("Enter language name:")
+
+d.update({name:lang})
+name=input("Enter friends name:")
+lang=input("Enter language name:")
+
+d.update({name:lang})
+name=input("Enter friends name:")
+lang=input("Enter language name:")
+
+d.update({name:lang})
+name=input("Enter friends name:")
+lang=input("Enter language name:")
+
+print(d)
