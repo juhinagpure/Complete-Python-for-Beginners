ccf2ad60fca784f5bdeef5216e4c93fc6d9d973c
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-05-29 18:43:50 +0530

    Enhance age eligibility example in chapter 6 with additional output messages

--- chapter6/01_conditionals.py
+++ chapter6/01_conditionals.py
@@ -2,5 +2,9 @@ a=int(input("Enter your age "))
 
 if(a>18):
     print("You are eligible to vote")
+    print("Good for you")
+
 else:
     print("You are not eligible to vote")
+
+print("End of program")
