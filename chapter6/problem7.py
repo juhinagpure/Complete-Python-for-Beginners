b9484fbe7edaeeaa52043309af24e7ed15a006ab
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-05-31 09:02:08 +0530

    Add presence check for the name "harry" in user input post in chapter 6

---
+++ chapter6/problem7.py
@@ -0,0 +1,7 @@
+# post="Hey harry is good and harry is great"
+post = input("Enter a post: ")
+
+if("harry".lower() in post.lower()):
+    print("Yes, harry is present in the post")
+else:
+    print("No, harry is not present in the post")
