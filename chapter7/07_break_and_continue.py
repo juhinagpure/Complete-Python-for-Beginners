7103d46b9a03c6da82c2323ba784e368c3fddc61
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-05-31 11:27:29 +0530

    Add examples of break and continue statements in loops in chapter 7

--- chapter7/07_break_and_continue.py
+++ chapter7/07_break_and_continue.py
@@ -1,4 +1,11 @@
+#break statement in a loop
 for i in range(100):
     if(i==34):
         break # This will stop the loop when i is 34
+    print(i)
+
+# continue statement in a loop
+for i in range(100):
+    if(i==34):
+        continue # This will skip the iteration when i is 34
     print(i)
