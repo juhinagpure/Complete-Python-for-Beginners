5dd3d2050c73f7dcbad848b89208f432456a090a
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-05-31 08:42:37 +0530

    Add user registration check example in chapter 6

---
+++ chapter6/problem5.py
@@ -0,0 +1,8 @@
+l=["Harry","Ravi","Sita","Ram"]
+
+name=input("Enter your name:")
+
+if(name in l):
+    print("Welcome back", name)
+else:
+    print("You are not registered, please register first")
