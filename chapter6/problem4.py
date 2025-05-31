a6828685531597783abe073c347c1aaeff3ffe61
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-05-31 08:40:06 +0530

    Add username length validation example in chapter 6

---
+++ chapter6/problem4.py
@@ -0,0 +1,7 @@
+username=input("Enter your username: ")
+
+if(len(username)<10):
+    print("your username contains less than 10 characters")
+
+else:
+    print("your username contains more than 10 characters")
