2a7909f9204e81800d20e7b91edff993d38f1c69
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-06-02 20:45:26 +0530

    Add goodDay function examples with various argument handling in chapter 8

---
+++ chapter8/03_function_with_arguments.py
@@ -0,0 +1,12 @@
+def goodDay(name):
+    print("Good day ,"+ name)
+
+goodDay("juhi")
+
+def goodDay(name,ending):
+    print("Good day ,"+ name)
+    print(ending)
+
+goodDay("juhi","Have a nice day!")
+goodDay("shubham","Have a nice day!")
+goodDay("rohan","Thank you for coming!")
