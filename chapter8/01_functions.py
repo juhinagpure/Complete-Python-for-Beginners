40fed28f8d0b87665ba1aa1099448e668de793f3
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-06-02 11:18:24 +0530

    Add average calculation function in chapter 8

---
+++ chapter8/01_functions.py
@@ -0,0 +1,17 @@
+# a=int(input("Enter first number: "))
+# b=int(input("Enter first number: "))
+# c=int(input("Enter first number: "))
+
+# average=(a+b+c)/3
+# print(average)
+
+
+def avg():
+    a=int(input("Enter first number: "))
+    b=int(input("Enter first number: "))
+    c=int(input("Enter first number: "))
+
+    average=(a+b+c)/3
+    print(average)
+
+avg()
