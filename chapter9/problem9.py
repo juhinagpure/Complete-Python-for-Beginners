b3ee078e33d95fdd28348e977667ce625f77376d
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-06-05 23:13:00 +0530

    Add file comparison functionality in problem9.py

---
+++ chapter9/problem9.py
@@ -0,0 +1,9 @@
+with open("chapter9/file.txt") as f:
+    content1= f.read()
+with open("chapter9/poem.txt") as f:
+    content2= f.read()
+if content1 == content2:
+    print("Both files are same")
+else:
+    print("Files are not same")
+    
