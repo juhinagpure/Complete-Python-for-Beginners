28b0b2e5437868a0fd29240105117a60239def0c
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-06-05 23:15:50 +0530

    Add commented-out code for file comparison in problem9.py

--- chapter9/problem9.py
+++ chapter9/problem9.py
@@ -6,4 +6,13 @@ if content1 == content2:
     print("Both files are same")
 else:
     print("Files are not same")
+
+# with open("chapter9/this.txt") as f:
+#     content1= f.read()
+# with open("chapter9/this_copy.txt") as f:
+#     content2= f.read()
+# if content1 == content2:
+#     print("Both files are same")
+# else:
+#     print("Files are not same")
     
