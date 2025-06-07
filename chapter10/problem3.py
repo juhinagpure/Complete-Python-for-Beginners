91ae3b308e05a29a56b924dfcae2d2a36dc4311a
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-06-07 11:16:19 +0530

    Enhance Demo class with comments explaining class and instance attributes

--- chapter10/problem3.py
+++ chapter10/problem3.py
@@ -1,6 +1,15 @@
 class Demo:
-    a=4
-o=Demo()
-print(o.a)
-o.a=0
-print(o.a)
+    a = 4  # Class attribute
+
+# Creating an object of the class
+o = Demo()
+
+# Since 'a' is not defined in the object yet, it prints the class attribute
+print(o.a)  # Output: 4
+
+# Now we assign a new value to 'a' using the object 'o'
+# This creates an instance attribute 'a' that overrides the class attribute for this object
+o.a = 0
+
+# Now, it prints the instance (object) attribute 'a', not the class one
+print(o.a)  # Output: 0
