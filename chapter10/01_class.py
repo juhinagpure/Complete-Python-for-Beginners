59ce39e169732616607331796cc985dcd2d85c19
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-06-07 08:36:39 +0530

    Enhance Employee class to demonstrate object and class attributes with additional print statements

--- chapter10/01_class.py
+++ chapter10/01_class.py
@@ -1,8 +1,18 @@
 class Employee:
     name = "Harry"
-    language = "Python"
+    language = "Python" # class attribute
     salary = 1200000
 
 harry = Employee()
-print(harry.name, harry.language)
-print(harry.salary)
+harry.name="Harry" # object attribute
+print(harry.name, harry.language, harry.salary)
+
+
+rohan=Employee()
+rohan.name = "Rohan"
+print(rohan.name, rohan.language, rohan.salary)
+
+# Here name is object attribute and salary and language are language are class attributes as they directly belong to the class
+
+
+    
