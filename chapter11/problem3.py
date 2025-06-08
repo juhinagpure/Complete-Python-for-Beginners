185eb725f0662ac6465cd0336f8a268a40da3929
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-06-08 16:55:21 +0530

    Refactor Employee class to use instance variables for salary and increment, and add setter for increment calculation

--- chapter11/problem3.py
+++ chapter11/problem3.py
@@ -1,11 +1,19 @@
 class Employee:
-    salary=234
-    increment=20
+    def __init__(self):
+        self._salary = 234
+        self._increment = 20
 
     @property
     def salaryAfterIncrement(self):
-        return (self.salary+self.salary+(self.increment/100))
+        return self._salary * (1 + self._increment / 100)
 
+    @property
+    def increment(self):
+        return self._increment
 
-e=Employee()
-print(e.salaryAfterIncrement)  # This will print the salary after increment
+    @increment.setter
+    def increment(self, new_salary):
+        self._increment = (new_salary / self._salary - 1) * 100
+
+e = Employee()
+print(e.salaryAfterIncrement)  # Output: 280.8
