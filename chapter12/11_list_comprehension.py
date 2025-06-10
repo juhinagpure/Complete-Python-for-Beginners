a0af16a0ca377f96b2b872137cd1c1f7c6633ca5
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-06-10 16:45:07 +0530

    Replace traditional loop with list comprehension for squaring values in myList

--- chapter12/11_list_comprehension.py
+++ chapter12/11_list_comprehension.py
@@ -1,6 +1,10 @@
 myList=[1,2,5,9,3,5,5]
 
-squaredList = []
-for item in myList:
-    squaredList.append(item**2)
+# squaredList = []
+# for item in myList:
+#     squaredList.append(item**2)
+
+# Using list comprehension to create a new list with squared values
+squaredList=[i*i for i in myList]
+
 print(squaredList)
