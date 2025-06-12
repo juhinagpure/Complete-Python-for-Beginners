c3f6fc7dfbecefa2de975111d68cce64c2ad2b57
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-06-12 15:44:02 +0530

    Add filter example to demonstrate usage of map and filter functions

--- chapter13/05_map_filter_reduce.py
+++ chapter13/05_map_filter_reduce.py
@@ -1,6 +1,15 @@
+# Map 
 l=[1,2,3,4,5]
 
 square=lambda x:x*x
 
 sqList=map(square,l)
 print(list(sqList))
+
+# Filter
+def even(n):
+    if(n%2==0):
+        return True
+    return False
+onlyEven=filter(even,l)
+print(list(onlyEven))
